f110381d882f29ca2e3413138f9858a31f8cc49d
Antonio Villalobos <antoniovillalobos@gmail.com>
2023-11-18 16:51:04 -0300

    movie relation and validations created

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :lists, through: :bookmarks
+
+  validates :title, :overview, presence: true
+  validates :title, uniqueness: true
 end
